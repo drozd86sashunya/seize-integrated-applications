98268e8e51b6357830f667e4a514818144eef2a5
drozd86sashunya <152299288+drozd86sashunya@users.noreply.github.com>
2023-12-13 08:04:35 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,66 @@
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    for item in data:
+def generate_random_data():
+
+
+if __name__ == "__main__":
+    main()
+def main():
+        print(f"Random Number: {item}")
+    data = generate_random_data()if __name__ == "__main__":
+import random
+def main():
+    main()
+    data = generate_random_data()
+
+
+
+
+def generate_random_data():
+    return data
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")    data = generate_random_data()
+if __name__ == "__main__":
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+    return data
+def main():
+def generate_random_data():
+    main()
+        print(f"Random Number: {item}")
+import randomdef main():
+    main()
+
+    for item in data:
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    data = generate_random_data()
+import random
+
+        print(f"Random Number: {item}")
+    return data    for item in data:
+def main():
+
+
+        print(f"Random Number: {item}")
+    return data
+    data = generate_random_data()
+import random
+
+    main()
+def generate_random_data():
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
